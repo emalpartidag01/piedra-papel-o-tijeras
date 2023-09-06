5f0f36ffee2417d15f7c375fc2bf266f402659a0
emalpartidag01 <144253704+emalpartidag01@users.noreply.github.com>
2023-09-06 17:14:35 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ input.onGesture(Gesture.Shake, function () {
             # . . . #
             # # # # #
             `)
+        music.play(music.createSoundExpression(WaveShape.Noise, 5000, 1, 150, 0, 779, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     } else if (MANO == 2) {
         basic.showLeds(`
             . . . . .
@@ -17,6 +18,7 @@ input.onGesture(Gesture.Shake, function () {
             . # # # .
             . . . . .
             `)
+        music.play(music.createSoundExpression(WaveShape.Sine, 31, 4, 223, 223, 238, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     } else {
         basic.showLeds(`
             # # . . #
@@ -25,5 +27,6 @@ input.onGesture(Gesture.Shake, function () {
             # # . # .
             # # . . #
             `)
+        music.play(music.createSoundExpression(WaveShape.Noise, 523, 1, 255, 0, 100, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), music.PlaybackMode.UntilDone)
     }
 })
