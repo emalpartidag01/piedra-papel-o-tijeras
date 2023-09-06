d4ac1f2c61dd6336c7a9cb85edc6244640338c7e
emalpartidag01 <144253704+emalpartidag01@users.noreply.github.com>
2023-09-06 17:16:53 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -27,6 +27,6 @@ input.onGesture(Gesture.Shake, function () {
             # # . # .
             # # . . #
             `)
-        music.play(music.createSoundExpression(WaveShape.Noise, 523, 1, 255, 0, 100, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), music.PlaybackMode.UntilDone)
+        music.play(music.createSoundExpression(WaveShape.Noise, 54, 54, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
     }
 })
