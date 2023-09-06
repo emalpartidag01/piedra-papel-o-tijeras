a31e14391fd69d192f65dec761000d97c970d850
emalpartidag01 <144253704+emalpartidag01@users.noreply.github.com>
2023-09-06 16:45:32 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+let MANO = 0
+input.onGesture(Gesture.Shake, function () {
+    MANO = randint(1, 3)
+    if (MANO == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (MANO == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
